089ac863b96a68065161ec95b41f07bf3cf8d04f
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-10-29 12:48:02 +0100

    first commit

---
+++ app/components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <header className="h-screen flex justify-center items-center">
+      <div>
+        <h1 className="text-8xl font-semi-bold tracking-tighter">
+          Dawid Zurawski
+        </h1>
+        <p className="font-light mt-2">
+          Frontend. Finance. Everything fintech.
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
